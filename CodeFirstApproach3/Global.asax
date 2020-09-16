4eee95638544f77120d2aa53509d4cb5cb6bb583
Administrator <Administrator@ANGEL-PC>
2020-09-16 09:48:16 +0530

    Add project files.

---
+++ CodeFirstApproach3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeFirstApproach3.MvcApplication" Language="C#" %>
---
+++ CodeFirstApproach3/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstApproach3.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Name is a required field")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage ="Mandatory to fill RegNo")]
+        public String RegNo { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Invalid Email")]
+        public String Email { get; set; }
+
+        public String Address { get; set; }
+    }
+}
